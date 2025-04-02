b8fa4cfd5922db34af84c44200002bd74def7b41
grzegorzstatkiewicz <grzegorz.statkiewicz@gmail.com>
2025-04-02 15:54:04 +0200

    Comment empty describe

--- src/web/src/app/shared/components/entry-instance/entry-instance.component.spec.ts
+++ src/web/src/app/shared/components/entry-instance/entry-instance.component.spec.ts
@@ -1,26 +1,26 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EntryInstanceComponent } from './entry-instance.component';
+// import { EntryInstanceComponent } from './entry-instance.component';
 
-describe('EntryInstanceComponent', () => {
-  let component: EntryInstanceComponent;
-  let fixture: ComponentFixture<EntryInstanceComponent>;
+// describe('EntryInstanceComponent', () => {
+//   let component: EntryInstanceComponent;
+//   let fixture: ComponentFixture<EntryInstanceComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [EntryInstanceComponent],
-    }).compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [EntryInstanceComponent],
+//     }).compileComponents();
 
-    fixture = TestBed.createComponent(EntryInstanceComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//     fixture = TestBed.createComponent(EntryInstanceComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
   // it('should create', () => {
   //   expect(component).toBeTruthy();
   // });
 
   // TODO: fix tests
-});
+// });
 
 
