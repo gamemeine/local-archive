e58c5fb44bfc67b61ec91b8412016d399cab659f
bpucilowski <pucilowskibartlomiej@gmail.com>
2025-04-05 16:52:02 +0200

    Setup auth0 api

--- src/api/main.py
+++ src/api/main.py
@@ -1,13 +1,16 @@
 from environment import setup
-from fastapi import FastAPI
+from fastapi import Security, FastAPI
+from fastapi.security import HTTPBearer
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import create_engine
+from utils import VerifyToken
 from db.db import get_database
 from db.models import Base
 
 
 setup()
 app = FastAPI()
+auth = VerifyToken()
 
 app.add_middleware(
     CORSMiddleware,
@@ -46,3 +49,21 @@ async def get_data():
     query = "SELECT * FROM items"
     result = await database.fetch_all(query)
     return result
+
+
+@app.get("/api/public")
+def public():
+    """No access token required to access this route"""
+
+    result = {
+        "status": "success",
+        "msg": ("Hello from a public endpoint! You don't need to be "
+                "authenticated to see this.")
+    }
+    return result
+
+
+@app.get("/api/private")
+def private(auth_result: str = Security(auth.verify)):
+    """A valid access token is required to access this route"""
+    return auth_result
