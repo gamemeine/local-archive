2c58adb39f3b6245e51fc07d3783062e31e5a009
Jakub Kryczka <kryczka.jakub.pg2@gmail.com>
2025-04-18 09:48:44 +0200

    Fixed map style import

--- src/web/src/app/app.component.ts
+++ src/web/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss','/node_modules/mapbox-gl/dist/mapbox-gl.css']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'LocalArchive';
