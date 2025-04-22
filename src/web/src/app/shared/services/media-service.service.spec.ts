e07fc57aa3575bdbf36b09c4bb3738c54f59f8f9
grzegorzstatkiewicz <grzegorz.statkiewicz@gmail.com>
2025-04-22 22:15:35 +0200

    Fix tests

--- src/web/src/app/shared/services/media-service.service.spec.ts
+++ src/web/src/app/shared/services/media-service.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MediaServiceService } from './media-service.service';
 
 describe('MediaServiceService', () => {
   let service: MediaServiceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(MediaServiceService);
   });
 
