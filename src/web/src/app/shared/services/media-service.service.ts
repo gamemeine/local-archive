2f9e4c2e6727251b73dc2f9d688868b3e4b9bd2c
bpucilowski <pucilowskibartlomiej@gmail.com>
2025-05-19 22:17:23 +0200

    Back to previous stuff deleted by mistake

--- src/web/src/app/shared/services/media-service.service.ts
+++ src/web/src/app/shared/services/media-service.service.ts
@@ -9,9 +9,28 @@ import { Media } from '../interfaces/media';
 export class MediaServiceService {
   constructor(private http: HttpClient) {}
 
-  search(params: any) {
-    // params should be the object with all search fields from the form
-    return this.http.post<Media[]>(`${environment.apiUrl}/search`, params);
+  search() {
+    let body = {
+      location: {
+        top_left: {
+          lon: 0,
+          lat: 90,
+        },
+        bottom_right: {
+          lon: 90,
+          lat: 0,
+        },
+      },
+      phrase: null,
+      creation_date: {
+        year_from: 2010,
+        year_to: 2020,
+      },
+      page: 0,
+      size: 10,
+    };
+
+    return this.http.post<Media[]>(`${environment.apiUrl}/search`, body);
   }
 
   uploadMedia(formData: FormData) {
