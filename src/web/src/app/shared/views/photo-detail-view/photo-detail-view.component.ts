35df560d3c560f15312c31c7e1b261363e4985ec
Karol Kuc <karolkuc1103@gmail.com>
2025-05-20 20:31:24 +0200

    Added new view for photo-detail

---
+++ src/web/src/app/shared/views/photo-detail-view/photo-detail-view.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-photo-detail-view',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './photo-detail-view.component.html',
+//   styleUrl: './my-photos-view.component.scss' TODO add this later
+})
+export class PhotoDetailViewComponent {
+
+}
